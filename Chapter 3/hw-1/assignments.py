7a5ec61f0297f9a2af3ff8258774e2098eb7e264
Amir <aemadzadeh@nvidia.com>
2017-08-03 14:06:56 -0700

    Using biult-in len function for lists.

--- Chapter 3/hw-1/assignments.py
+++ Chapter 3/hw-1/assignments.py
@@ -1,22 +1,39 @@
 
+# class MaxSizeList(object):
+
+# 	def __init__(self, max):
+# 		self.max = max
+# 		self.list = []
+# 		self.length = 0
+
+# 	def push(self, val):
+# 		if(self.length >= self.max):
+# 			self.list.pop(0)
+# 			self.length -= 1
+		
+# 		self.list.append(val)
+# 		self.length += 1
+		
+# 	def get_list(self):	
+# 		return self.list
+
+# 	def get_length(self):
+# 		return self.length
+
 class MaxSizeList(object):
 
 	def __init__(self, max):
 		self.max = max
 		self.list = []
-		self.length = 0
-
-	def push(self, val):
-		if(self.length >= self.max):
-			self.list.pop(0)
-			self.length -= 1
 		
+	def push(self, val):
+		if(len(self.list) >= self.max):
+			self.list.pop(0)
+			
 		self.list.append(val)
-		self.length += 1
 		
 	def get_list(self):	
 		return self.list
 
 	def get_length(self):
-		return self.length	
-
+		return len(self.list)
