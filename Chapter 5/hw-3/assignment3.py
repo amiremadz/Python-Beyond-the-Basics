330acbcf056af04dcd4e951da6b33b0a90e07e7e
Amir <aemadzadeh@nvidia.com>
2017-08-06 12:39:30 -0700

    Assignment 3: Enhanced dictionary.

--- Chapter 5/hw-3/assignment3.py
+++ Chapter 5/hw-3/assignment3.py
@@ -1,2 +1,24 @@
+
 class ConfigDict(dict):
-    pass
+    
+    def __init__(self, file_name):
+
+    	self._file_name = file_name
+
+    	try:
+    		with open(self._file_name) as file:
+    			for line in file:
+    				key, val = line.split('=', 1)
+    				super(ConfigDict, self).__setitem__(key, val)
+
+    	except:
+    		print 'There is no config file.'
+    		print 'Creating one.'
+    		self.file = open(file_name, 'w+')
+	    	self.file.close()
+
+    def __setitem__(self, key, val):
+    	super(ConfigDict, self).__setitem__(key, val)
+    	
+    	with open(self._file_name, 'a+') as file:
+    	   	file.write('{0}={1}\n'.format(key, val))
--- Chapter 5/hw-3/test.py
+++ Chapter 5/hw-3/test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
     Usages:
     ./test.py                       (reads out the entire config dict)
@@ -5,11 +6,20 @@
 """
 
 import sys
-from assigment3 import ConfigDict
+from assignment3 import ConfigDict
 
 cd = ConfigDict('config_file.txt')
 
 if len(sys.argv) == 3:
-    key = sys.argv[1]
-    value
 
+    key = sys.argv[1]
+    value = sys.argv[2]
+    print('writing data:  {0}, {1}'.format(key, value))
+
+    cd[key] = value
+
+else:
+
+    print('reading data')
+    for key in cd.keys():
+        print('{0} = {1}'.format(key, cd[key]))
